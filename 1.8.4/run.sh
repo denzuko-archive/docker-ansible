923f1f2753de0d1f7e508bd4ae2602b33dbf5bb3
Dwight Spencer <dspencer@computekindustries.com>
2015-03-27 17:23:04 -0500

    (feature) Supports loading three top ssh key types

--- 1.8.4/run.sh
+++ 1.8.4/run.sh
@@ -1,10 +1,20 @@
-#!/bin/sh
+#!/bin/bash
+# maintainer: dwightaspencer@computekindustries.com
+#
+# run.sh -  loads in either rsa, dsa, or ecdsa keys for ssh
 
 eval `ssh-agent -s` > /dev/null 2>&1
 
-for key in `ls /ssh/*_rsa`
-do
-  ssh-add ${key} > /dev/null 2>&1
-done
+add_keys() {
+    SSH_ROOT=${1}
+
+    find ${SSH_ROOT} \( -name \*_rsa -o -name \*_dsa -o -name \*_ecdsa \) -type f |\
+        xargs -I{} -- ssh-add ${key} > /dev/null 2>&1
+}
+
+[ -d /ssh ] && export SSH_ROOT=/ssh
+[ -d ${HOME}/.ssh ] && export SSH_ROOT=/ssh
+
+add_keys $SSH_ROOT
 
 exec "$@"
